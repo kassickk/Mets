322959d893cca85d8565633ff18bcfa0e182c43f
KassickK <KassickK@knights.local>
2019-05-09 08:58:54 -0400

    initial upload

---
+++ PrettyArtthatimade/main.py
@@ -0,0 +1,138 @@
+speed(0)
+color("orange")
+
+#part 1
+
+penup()
+right(90)
+setposition(140, 140)
+circle(10, 180)
+pendown()
+circle(27, 180)
+right(90)
+right(90)
+circle(27, 180)
+penup()
+right(90)
+
+#part 2
+
+forward(100)
+right(90)
+pendown()
+circle(27, 180)
+right(180)
+circle(27, 180)
+left(90)
+
+#part 3
+
+forward(30)
+right(90)
+forward(150)
+left(90)
+forward(50)
+left(90)
+forward(150)
+right(90)
+forward(30)
+penup()
+right(180)
+forward(30)
+left(90)
+forward(150)
+right(90)
+pendown()
+
+#part 4
+
+forward(80)
+left(90)
+circle(27, 180)
+left(180)
+circle(27, 180)
+left(90)
+forward(30)
+
+#part 5
+
+penup()
+setposition (140, 140)
+pendown()
+backward(20)
+forward(30)
+left(90)
+forward(150)
+right(90)
+forward(50)
+right(90)
+forward(150)
+left(90)
+forward(30)
+penup()
+left(180)
+forward(30)
+left(90)
+forward(150)
+right(90)
+setposition(140, 140)
+right(180)
+forward(10)
+left(90)
+forward(150)
+right(90)
+
+#part 6
+
+pendown()
+forward(80)
+left(90)
+circle(27, 180)
+left(180)
+circle(27, 180)
+left(90)
+forward(30)
+
+#part 7
+penup()
+forward(105)
+pendown()
+left(90)
+
+#part 8
+
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+left(180)
+forward(100)
+left(90)
+forward(50)
+right(180)
+
+#part nine
+forward(80)
+left(90)
+circle(27, 180)
+left(180)
+circle(27, 180)
+left(90)
+forward(30)
+
+#part 10
+penup()
+setposition(140, 140)
+forward(218)
+left(90)
+forward(10)
+pendown()
+left(50)
+forward(210)
+left(135)
+forward(20)
+left(43)
+forward(180)
+
+#part 11
